3de3b338f0bf4d037f9a88c8ba8a1e22e6548c44
A1yip <103228609+A1yip@users.noreply.github.com>
2023-11-08 02:18:32 -0800

    isSelected method

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -75,6 +75,15 @@ public class Recipe extends HBox {
     		_recipe.Select(false);
     	Select(true);
     }
+    public Recipe getSelected() {
+        for (Recipe _recipe: allRecipes) {
+            if (_recipe.isSelected == true) {
+                return _recipe;
+            }
+        }
+        return null;
+
+    }
 
     private void Select(boolean toSelect) {
         
