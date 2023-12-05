8abb546805c85502e3dc044517aac40f2d675815
SeaSponge <61109988+Squidwarder@users.noreply.github.com>
2023-12-05 14:36:12 -0800

    filled in test for sortA2Z

--- src/test/java/RecipeSortTests.java
+++ src/test/java/RecipeSortTests.java
@@ -4,43 +4,81 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.time.*;
 
 public class RecipeSortTests {
-	private Recipe recipe;
+	private Recipe recipe1;
+	private Recipe recipe2;
+	private Recipe recipe3;
+	private Recipe recipe4;
 	private ArrayList<Recipe> allRecipes;
+	private RecipeDetails blankDetails;
+	private RecipeListMock testRecipeList;
 	
 	@BeforeEach
 	void setUp() {
 		allRecipes = new ArrayList<Recipe>();
 		
 		// Created recipe for testing use
-		recipe = new Recipe();
-		recipe.setRecipeName("Ham and Cheese Sandwich");
-		recipe.updateRecipeArray(allRecipes);
-		allRecipes.add(recipe);
+		recipe1 = new Recipe();
+		recipe1.setRecipeName("Ham and Cheese Sandwich");
+		
+		recipe2 = new Recipe();
+		recipe2.setRecipeName("Breakfast Cereal");
+
+		recipe3 = new Recipe();
+		recipe3.setRecipeName("Steak Shawarma");
+
+		recipe4 = new Recipe();
+		recipe4.setRecipeName("Chicken Kabob");
+
+		allRecipes.add(recipe1);
+		allRecipes.add(recipe2);
+		allRecipes.add(recipe3);
+		allRecipes.add(recipe4);
+
+		recipe1.updateRecipeArray(allRecipes);
+		recipe2.updateRecipeArray(allRecipes);
+		recipe3.updateRecipeArray(allRecipes);
+		recipe4.updateRecipeArray(allRecipes);
+
+		blankDetails = new RecipeDetails();
+		testRecipeList = new RecipeListMock(blankDetails, allRecipes);
 	}
 	
 	@Test
 	void testCreateRecipe() {
 		//Check recipe has been created
-		String name = "";
-		for (Recipe rec: allRecipes) {
-			if(rec.getRecipeName() == recipe.getRecipeName())
-				name = rec.getRecipeName();
-		}
-		assertEquals("Ham and Cheese Sandwich", name);
+		String name0 = allRecipes.get(0).getRecipeName();
+		String name1 = allRecipes.get(1).getRecipeName();
+		String name2 = allRecipes.get(2).getRecipeName();		
+		String name3 = allRecipes.get(3).getRecipeName();
+
+		assertEquals("Ham and Cheese Sandwich", name0);
+		assertEquals("Breakfast Cereal", name1);
+		assertEquals("Steak Shawarma", name2);
+		assertEquals("Chicken Kabob", name3);
 	}
 	
 	@Test 
 	void testSortA2Z() {
-		// String type = recipe.getDetailsMOCK("Ham and Cheese Sandwich", "type");
-		// assertEquals("Dinner", type);
+		
+		testRecipeList.recipeSortA2Z();
+		ArrayList<Recipe> sortedRecipes = testRecipeList.getAllRecipes();
+		String name0 = sortedRecipes.get(0).getRecipeName();
+		String name1 = sortedRecipes.get(1).getRecipeName();
+		String name2 = sortedRecipes.get(2).getRecipeName();		
+		String name3 = sortedRecipes.get(3).getRecipeName();
+
+		assertEquals("Breakfast Cereal", name0);
+		assertEquals("Chicken Kabob", name1);
+		assertEquals("Ham and Cheese Sandwich", name2);		
+		assertEquals("Steak Shawarma", name3);		
 	}
 	
 	@Test 
 	void testSortZ2A() {
-		// String ingredients = recipe.getDetailsMOCK("Ham and Cheese Sandwich", "ingredients");
-		// assertEquals("Ham, bread, cheese, mayo, mustard, oil, vinegar.", ingredients);
+
 	}
 	
 	@Test
